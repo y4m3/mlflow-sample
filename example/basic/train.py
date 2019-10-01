18a4a729a9d89bae8ff1ebb50e3591557764f905
y4m3 <y4m3@users.noreply.github.com>
2019-10-01 12:00:30 +0900

    update comments

--- example/basic/train.py
+++ example/basic/train.py
@@ -3,12 +3,14 @@
 
 import mlflow
 
-'''start log
+'''start mlflow session
 mlflow.start_run(run_id=None, experiment_id=None, run_name=None, nested=False)
 
-You don't have to worry about these arguments normally :)
-If you want to use arguments, see reference.
+You don't have to worry about these arguments normally. If you want to define
+run_id, experiment_id and run_name explicitly and start your experiments nested
+stracture. See reference.
 https://www.mlflow.org/docs/latest/python_api/mlflow.html#mlflow.start_run
+
 You can also use syntax like this:
 >>> with mlflow.start_run() as run:
 >>>     ...
@@ -35,7 +37,7 @@ mlflow.log_metric('metric_01', 0.234)
 mlflow.log_metric('metric_02', 0.567)
 
 
-'''log metrics like TensorBoard
+'''log metric progress like TensorBoard
 mlflow.log_metric("set metric name", "set metric value", "set step")
 
 You can log metrics like TensorBoard using log_metric function with "step".
@@ -44,7 +46,7 @@ for step, value in enumerate([0.6, 0.4, 0.2, 0.15, 0.13, 0.17]):
     mlflow.log_metric('loss', value, step)
 
 
-'''end log
+'''end mlflow session
 mlflow.end_run()
 
 If you use "with" syntax, this function do not need to type.
